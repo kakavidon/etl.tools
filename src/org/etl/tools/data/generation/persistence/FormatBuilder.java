cc014d50eb05fd2a53c4dc19514e41d60a3db739
kakavidon <kakavidon@gmail.com>
2017-09-07 18:41:43 +0300

    zxx

--- src/org/etl/tools/data/generation/persistence/FormatBuilder.java
+++ src/org/etl/tools/data/generation/persistence/FormatBuilder.java
@@ -2,6 +2,9 @@ package org.etl.tools.data.generation.persistence;
 
 public interface FormatBuilder {
 
+	
+	
+	
 	public <T> PersistenceProvider<T> create(String fileName);
 
 	public String getClassName();
