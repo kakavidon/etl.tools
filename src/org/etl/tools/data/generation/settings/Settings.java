c25ad9523bca9a9846bf46d551afeeae29b887b4
kakavidon <kakavidon@gmail.com>
2017-09-07 19:08:30 +0300

    Todo

--- src/org/etl/tools/data/generation/settings/Settings.java
+++ src/org/etl/tools/data/generation/settings/Settings.java
@@ -3,7 +3,8 @@ package  org.etl.tools.data.generation.settings;
 public class Settings {
 
 	public static String getPersistenceProvider() {
-		// TODO Auto-generated method stub
+		// TODO Retractor : Some of the base settings should be in properties file
+		
 		return "org.etl.tools.data.generation.persistence.JsonProvider";
 	}
 
