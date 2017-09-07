e988fd3bfc068ea533ff7142a312e65d478592ff
kakavidon <kakavidon@gmail.com>
2017-09-07 18:45:07 +0300

    chepo

--- src/org/etl/tools/data/generation/persistence/FormatBuilder.java
+++ src/org/etl/tools/data/generation/persistence/FormatBuilder.java
@@ -2,7 +2,9 @@ package org.etl.tools.data.generation.persistence;
 
 public interface FormatBuilder {
 
-	
+	/**
+	 * Git sucks cocks
+	 */
 	
 	
 	public <T> PersistenceProvider<T> create(String fileName);
