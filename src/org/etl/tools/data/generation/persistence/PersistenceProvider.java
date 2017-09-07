0e7b4c312c975e16a577aea5aecd30cf80924bf3
Lachezar.Nedelchev <Lachezar.Nedelchev@IALEKSANDROVA.adastracorp.net>
2017-09-07 17:50:00 +0300

    Hoi

--- src/org/etl/tools/data/generation/persistence/PersistenceProvider.java
+++ src/org/etl/tools/data/generation/persistence/PersistenceProvider.java
@@ -1,8 +1,36 @@
 package org.etl.tools.data.generation.persistence;
 
+/**
+ * 
+ * Interface that declares persistent operations (read and write) to an object
+ * of type T.
+ * 
+ * @author Lachezar.Nedelchev
+ *
+ * @param <T>
+ *            The type of the object that is persisted/read.
+ */
 public interface PersistenceProvider<T> {
-	
-	T read(Class<T> type) throws PersistenceException;
+	/**
+	 * 
+	 * Reads a object of type T to a file.
+	 * 
+	 * @param type
+	 *            The type of the object
+	 * @return An object of a type T if successfully read.
+	 * @throws PersistenceException
+	 *             when reading fails.
+	 */
+	public T read(Class<T> type) throws PersistenceException;
 
-	void write(T object) throws PersistenceException;
+	/**
+	 * 
+	 * Writes a object of type T to a file.
+	 * 
+	 * @param object
+	 *            The object to be written.
+	 * @throws PersistenceException
+	 *             When writing fails.
+	 */
+	public void write(T object) throws PersistenceException;
 }
