02117e633e8a3e2848e3cc6406f66185ccd37852
kakavidon <kakavidon@gmail.com>
2017-09-07 17:29:38 +0300

    Add files via upload
    Initial Version.

---
+++ src/org/etl/tools/data/generation/persistence/FormatBuilder.java
@@ -0,0 +1,8 @@
+package org.etl.tools.data.generation.persistence;
+
+public interface FormatBuilder {
+
+	public <T> PersistenceProvider<T> create(String fileName);
+
+	public String getClassName();
+}
